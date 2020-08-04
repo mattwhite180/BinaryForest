a77817bdab2106dba343e2254b963cab92c362cd
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2020-08-04 14:21:16 -0600

    added springboot app

---
+++ BinaryForest/src/main/java/mattwhite180/BinaryForest/BinaryForestApplication.java
@@ -0,0 +1,13 @@
+package mattwhite180.BinaryForest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaryForestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinaryForestApplication.class, args);
+	}
+
+}
---
+++ BinaryForest/src/test/java/mattwhite180/BinaryForest/BinaryForestApplicationTests.java
@@ -0,0 +1,13 @@
+package mattwhite180.BinaryForest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinaryForestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
