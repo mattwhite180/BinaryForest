35c5b680c654af93ff3f56f4bbe11f12d7a70bb0
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2020-08-05 13:23:33 -0600

    renamed hello view to helloyou

--- BinaryForest/src/main/java/mattwhite180/servingwebcontent/HelloYouController.java
+++ BinaryForest/src/main/java/mattwhite180/servingwebcontent/HelloYouController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloYouController {
 
-	@GetMapping("/hello")
+	@GetMapping("/helloyou")
 	public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
 		return "hello";
