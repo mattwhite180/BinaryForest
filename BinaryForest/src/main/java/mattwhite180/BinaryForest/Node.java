2a5574b03e19cfacc653f5a76ecda3b46a18564f
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2020-08-05 16:59:07 -0600

    added Node.java

---
+++ BinaryForest/src/main/java/mattwhite180/BinaryForest/Node.java
@@ -0,0 +1,13 @@
+package mattwhite180.BinaryForest;
+
+public class Node {
+    int value;
+    Node left;
+    Node right;
+ 
+    Node(int value) {
+        this.value = value;
+        right = null;
+        left = null;
+    }
+}
