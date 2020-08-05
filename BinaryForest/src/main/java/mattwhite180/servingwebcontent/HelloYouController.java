ef71315e7b234af09e381691b62fbc071b08eeee
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2020-08-05 11:48:27 -0600

    made form

--- BinaryForest/src/main/java/mattwhite180/servingwebcontent/HelloYouController.java
+++ BinaryForest/src/main/java/mattwhite180/servingwebcontent/HelloYouController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class GreetingController {
+public class HelloYouController {
 
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+	@GetMapping("/hello")
+	public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
-		return "greeting";
+		return "hello";
 	}
 
 }
