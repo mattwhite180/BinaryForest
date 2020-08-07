cbe68b664b10914312c351b19c8c5c48fabf5041
mattwhite180 <matthew.white42@aggiemail.usu.edu>
2020-08-07 09:01:23 -0600

    removed unused import statement

--- BinaryForest/src/test/java/mattwhite180/BinaryForest/BinaryTreeTest.java
+++ BinaryForest/src/test/java/mattwhite180/BinaryForest/BinaryTreeTest.java
@@ -4,8 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.ArrayList;
-
 import org.junit.jupiter.api.Test;
 
 class BinaryTreeTest {
